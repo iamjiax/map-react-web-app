5f1f28c91f75a8056de38900e5292d60beaba364
Jia Xu <iamjiax@gmail.com>
2022-11-15 12:50:05 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <div className="container">
+          <h1>Web Development Project</h1>
+          <Routes>
+            <Route/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+  );
+}
+
+export default App;
