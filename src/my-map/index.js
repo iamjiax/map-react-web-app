4065ad3203db8842c71f98b666f7dfa328654c34
Jia Xu <iamjiax@gmail.com>
2022-11-15 14:12:58 -0800

    events-location marker

--- src/my-map/index.js
+++ src/my-map/index.js
@@ -1,4 +1,5 @@
 import {MapContainer, Marker, Popup, TileLayer, useMap} from 'react-leaflet';
+import LocationMarker from "./location-marker";
 
 function MyMap() {
   return(
@@ -7,11 +8,7 @@ function MyMap() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <LocationMarker/>
       </MapContainer>
   )
 };
