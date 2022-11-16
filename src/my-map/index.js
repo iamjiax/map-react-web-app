eedfdadb17fcf5b0bdc65f0a6cc66f5e07883ef6
Jia Xu <iamjiax@gmail.com>
2022-11-15 23:38:51 -0800

    events-click & loactionfound

--- src/my-map/index.js
+++ src/my-map/index.js
@@ -1,9 +1,9 @@
-import {MapContainer, Marker, Popup, TileLayer, useMap} from 'react-leaflet';
+import {MapContainer, TileLayer} from 'react-leaflet';
 import LocationMarker from "./location-marker";
 
 function MyMap() {
-  return(
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  return (
+      <MapContainer center={[47.6, -122.3]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
