fcb7348291468f5137d9eafb8cea0e11b50b9676
yuanyuan <wu.yuanyu@northeastern.edu>
2022-11-26 01:20:13 -0800

    initial

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from 'aos';
 import {Constants} from "../util/constants";
 
 export const createUser = async (user) => {
@@ -10,3 +10,21 @@ export const findUser = async () => {
   const response = await axios.get(Constants.USER_API);
   return response.data;
 }
+
+export const register = async (user) => {
+  const response = await axios.post(Constants.USER_API, user)
+  return response.data
+}
+
+export const login = async (user) => {
+  const response = await axios.post(Constants.USER_API, user)
+  return response.data
+}
+
+export const logout = async () => {
+  const response = await axios.post(Constants.USER_API)
+  return response.data
+}
+
+export const deleteUser = async (uid) => {}
+export const updateUser = async (uid, userUpdates) => {}
--- src/services/user-thunks.js
+++ src/services/user-thunks.js
@@ -1,3 +1,23 @@
-import {createAsyncThunk} from "@reduxjs/toolkit";
-import * as userService from "./user-service";
 
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import {findUser, register, login, logout} from "./user-service";
+
+export const findUserThunk = createAsyncThunk(
+    'findUser',
+    async () => await findUser()
+)
+
+export const registerThunk = createAsyncThunk(
+    'register',
+    async (user) => await register(user)
+)
+
+export const logoutThunk = createAsyncThunk(
+    'logout',
+    async () => await logout()
+)
+
+export const loginThunk = createAsyncThunk(
+    'login',
+    async (user) => await login(user)
+)
