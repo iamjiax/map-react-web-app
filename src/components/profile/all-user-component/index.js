734d05031f62260766d2f77d4b9d0339d4c0985d
sshao <sshao1126@gmail.com>
2022-12-06 23:46:22 -0800

    update all user component

--- src/components/profile/all-user-component/index.js
+++ src/components/profile/all-user-component/index.js
@@ -1,34 +1,40 @@
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
-
-import {deleteUserThunk, findAllUsersThunk} from "../../../services/user-thunks";
+import {deleteUserThunk, findAllUsersThunk,} from "../../../services/user-thunks";
+import {Link, useParams} from "react-router-dom";
+import {UserRoles} from "../../../util/user-roles";
 
 const UserList = () => {
   const {users} = useSelector(state => state.userReducer);
+  const {uid} = useParams();
   const dispatch = useDispatch();
-  // const deleteUserHandler = (uid) => {
-  //   dispatch(deleteUserThunk(uid));
-  // }
-
   useEffect(() => {
-    dispatch(findAllUsersThunk())
-  }, [])
+    dispatch(findAllUsersThunk(uid))
+  }, [uid])
   return(
-    <>
-      <h3>Users (total users: {users.length})</h3>
+    <div>
+      <h3>Manage your user list (total users: {users.length})</h3>
       <ul className="list-group">
         {
-          users.map((user) =>
-          <li className="list-group-item"
-              key={user._id}>
-              {user.username}
-            <i className="bi bi-x-lg float-end" onClick={() =>  dispatch(deleteUserThunk(user._id))}></i>
-          </li>
-          )
+          users.map(user => <UserProfileItem key={user._id} user={user}/>)
         }
       </ul>
-    </>
-  )
+    </div>
+  );
+};
+const UserProfileItem = ({user}) => {
+  const dispatch = useDispatch();
+  const {currentUser} = useSelector(state => state.userReducer);
+  const profileUrl = currentUser?._id === user._id ? "/profile" : `/profile/${user._id}`;
+  return (
+      <li className="list-group-item">
+        <Link to={profileUrl}>{user.username}: {user.role}</Link>
+        {(currentUser?._id !== user._id) &&
+            <button className="btn border-secondary rounded-pill bi bi-x-lg float-end" onClick={() => dispatch(deleteUserThunk(user._id))}>delete user</button>}
+        {(currentUser?._id === user._id) &&
+            <button className="btn border-secondary rounded-pill float-end">Unable to delete yourself</button>}
+      </li>
+  );
 }
 
 export default UserList;
